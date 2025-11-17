1298a925b69c1102c5c67b94da82401c744c6eb1
Christabel-Akpene <dc.akpene@gmail.com>
2025-11-17 22:08:23 +0000

    project-directories

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,21 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export default function Home() {
+const Home: React.FC = () => {
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
-    >
-
-    </div>
-  );
+    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h1>Airbnb Application Clone System</h1>
+      <button className="border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full">Get Started</button>
+    </main>
+  )
 }
+
+export default Home;
